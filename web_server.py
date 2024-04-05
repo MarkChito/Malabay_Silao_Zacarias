2aea34a4e825f2851c60dc3c35b79e599542b00e
MarkChito <00anteja23@gmail.com>
2024-04-06 02:48:56 +0800

    04-06-2024 02:48 AM

--- web_server.py
+++ web_server.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, jsonify, redirect
-import os
-import numpy as np
-from PIL import Image
-import importlib.util
+from tensorflow.lite.python.interpreter import Interpreter
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timezone
+from PIL import Image
+
+import numpy as np
 import shutil
+import os
 import cv2
 
 app = Flask(__name__)
@@ -16,23 +17,17 @@ db = SQLAlchemy(app)
 
 class My_Database(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     object_name = db.Column(db.String(255), nullable=False)
-
+    
     def __repr__(self):
         return "<Name %r>" % self.id
 
-pkg = importlib.util.find_spec('tflite_runtime')
+with app.app_context():
+    db.create_all()
 
-from tensorflow.lite.python.interpreter import Interpreter
-
-MODEL_NAME = "model"
-GRAPH_NAME = "detect.tflite"
-LABELMAP_NAME = "labelmap.txt"
-THRESHOLD = 0.5
-
-PATH_TO_CKPT = os.path.join(os.getcwd(), MODEL_NAME, GRAPH_NAME)
-PATH_TO_LABELS = os.path.join(os.getcwd(), MODEL_NAME, LABELMAP_NAME)
+PATH_TO_CKPT = os.path.join(os.getcwd(), "model", "detect.tflite")
+PATH_TO_LABELS = os.path.join(os.getcwd(), "model", "labelmap.txt")
 
 with open(PATH_TO_LABELS, 'r') as f:
     labels = [line.strip() for line in f.readlines()]
@@ -42,6 +37,7 @@ interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
+
 height = input_details[0]['shape'][1]
 width = input_details[0]['shape'][2]
 
@@ -52,10 +48,7 @@ input_std = 127.5
 
 outname = output_details[0]['name']
 
-if 'StatefulPartitionedCall' in outname:
-    boxes_idx, classes_idx, scores_idx = 1, 3, 0
-else:
-    boxes_idx, classes_idx, scores_idx = 0, 1, 2
+boxes_idx, classes_idx, scores_idx = 1, 3, 0
 
 @app.route('/')
 def index():
@@ -67,23 +60,15 @@ def detect():
 
 @app.route('/result', methods=['POST'])
 def result():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
-
     file = request.files['file']
 
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'})
-
     image_path = os.path.join('static/uploads', file.filename)
+
     file.save(image_path)
 
     detections = perform_detection(image_path)
 
-    image = Image.open(image_path)
-    image_width, image_height = image.size
-
-    return render_template('result.html', image_filename=file.filename, detections=detections, image_width=image_width, image_height=image_height)
+    return render_template('result.html', image_filename=file.filename, detections=detections)
 
 @app.route("/check_connection")
 def check_connection():
@@ -92,23 +77,21 @@ def check_connection():
 @app.route('/unlisted_images', methods=["POST", "GET"])
 def unlisted_images():
     if request.method == "POST":
-        post_object_name = request.form["object_name"]
+        object_name = request.form["object_name"]
         file = request.files['object_image']
 
-        new_data = My_Database(object_name=post_object_name)
-
         image_path = os.path.join('static/uploads/temp', file.filename)
+
         file.save(image_path)
 
-        try:
-            db.session.add(new_data)
-            db.session.commit()
+        data = My_Database(object_name=object_name)
 
-            copy_image(post_object_name, file.filename)
+        db.session.add(data)
+        db.session.commit()
 
-            return redirect("/unlisted_images")
-        except:
-            return "An error has occured.."
+        copy_image(object_name, file.filename)
+
+        return redirect("/unlisted_images")
     else:
         data = db.session.query(My_Database).group_by(My_Database.object_name).order_by(My_Database.date_created.desc()).all()
 
@@ -129,16 +112,18 @@ def view_images():
     return render_template('view_images.html', filenames=filenames, title=post_image_folder)
 
 def perform_detection(image_path):
-    image = Image.open(image_path)
-    image_rgb = image.convert('RGB')
-    image_resized = image_rgb.resize((width, height))
+    image = cv2.imread(image_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    imH, imW, _ = image.shape 
+    image_resized = cv2.resize(image_rgb, (width, height))
+    input_data = np.expand_dims(image_resized, axis=0)
 
     input_data = np.expand_dims(np.array(image_resized, dtype=np.uint8), axis=0)
 
     if floating_model:
         input_data = (np.float32(input_data) - input_mean) / input_std
 
-    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.set_tensor(input_details[0]['index'],input_data)
     interpreter.invoke()
 
     boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0]
@@ -148,15 +133,29 @@ def perform_detection(image_path):
     detections = []
 
     for i in range(len(scores)):
-        if (scores[i] > THRESHOLD) and (scores[i] <= 1.0):
-            ymin = int(max(1, (boxes[i][0] * height)))
-            xmin = int(max(1, (boxes[i][1] * width)))
-            ymax = int(min(height, (boxes[i][2] * height)))
-            xmax = int(min(width, (boxes[i][3] * width)))
+        if ((scores[i] > 0.5) and (scores[i] <= 1.0)):
+            ymin = int(max(1,(boxes[i][0] * imH)))
+            xmin = int(max(1,(boxes[i][1] * imW)))
+            ymax = int(min(imH,(boxes[i][2] * imH)))
+            xmax = int(min(imW,(boxes[i][3] * imW)))
+            
+            cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
 
             object_name = labels[int(classes[i])]
-            detections.append({'object_name': object_name, 'confidence': float(scores[i]), 'xmin': xmin, 'ymin': ymin, 'xmax': xmax, 'ymax': ymax})
+            label = '%s: %d%%' % (object_name, int(scores[i]*100))
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+            label_ymin = max(ymin, labelSize[1] + 10)
+            
+            cv2.rectangle(image, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
+            cv2.putText(image, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
 
+            detections.append({'object_name': object_name, 'confidence': int(scores[i] * 100), 'xmin': xmin, 'ymin': ymin, 'xmax': xmax, 'ymax': ymax})
+
+    image_fn = os.path.basename(image_path)
+    image_savepath = os.path.join(os.getcwd(), "static/uploads", image_fn)
+
+    cv2.imwrite(image_savepath, image)
+    
     return detections
 
 def copy_image(filename, filepath):
@@ -176,6 +175,7 @@ def copy_image(filename, filepath):
         os.makedirs(folder_path)
     
     image_count = 0
+    
     for _, _, files in os.walk(folder_path):
         for file in files:
             if file.startswith("image_"):
@@ -183,14 +183,13 @@ def copy_image(filename, filepath):
                 image_count = max(image_count, index)
 
     image_count += 1
+    
     new_image_name = f"image_{image_count:04d}.jpg"
-    shutil.copy("static/uploads/temp/" + filepath, os.path.join(folder_path, new_image_name))
 
-def create_tables():
-    with app.app_context():
-        db.create_all()
+    shutil.copy("static/uploads/temp/" + filepath, os.path.join(folder_path, new_image_name))    
+
+def insert_db_data(data):
+    pass
 
 if __name__ == '__main__':
-    create_tables()
-
     app.run(host='0.0.0.0', port=3000, debug=True)
