db0a92b0edf2606bec7b0fe422d062daabb9c8b7
MarkChito <00anteja23@gmail.com>
2024-04-07 05:00:04 +0800

    04-04-2024 04:59 AM

--- database.py
+++ database.py
@@ -8,16 +8,76 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 db = SQLAlchemy(app)
 
-class Database(db.Model):
+class Unlisted_Images(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     object_name = db.Column(db.String(255), nullable=False)
     
     def __repr__(self):
-        return "<Name %r>" % self.id
+        return f"<Unlisted_Images id={self.id}>"
     
-    def insert(self, data):
+    @classmethod
+    def insert(cls, data):
         db.session.add(data)
+
+        db.session.commit()
+
+    @classmethod
+    def update(cls, old_name, new_name):
+        data = cls.query.filter_by(object_name=old_name).all()
+        
+        for item in data:
+            item.object_name = new_name
+        
+        db.session.commit()
+
+    @classmethod
+    def delete(cls, folder_name):
+        data = cls.query.filter_by(object_name=folder_name).all()
+        
+        for item in data:
+            db.session.delete(item)
+        
+        db.session.commit()
+    
+    @classmethod
+    def is_record_available(cls, folder_name):
+        data = cls.query.filter_by(object_name=folder_name).first()
+        
+        if data:
+            return True
+        
+        return False
+
+class Contact_Us_Messages(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    subject = db.Column(db.String(255), nullable=False)
+    message = db.Column(db.String(255), nullable=False)
+    
+    def __repr__(self):
+        return "<Contact_Us_Messages %r>" % self.id
+    
+    @classmethod
+    def insert(cls, data):
+        db.session.add(data)
+
+        db.session.commit()
+
+class Newsletter_List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    email = db.Column(db.String(255), nullable=False)
+    
+    def __repr__(self):
+        return "<Newsletter_List %r>" % self.id
+    
+    @classmethod
+    def insert(cls, data):
+        db.session.add(data)
+
         db.session.commit()
 
 with app.app_context():
--- web_server.py
+++ web_server.py
@@ -1,9 +1,10 @@
+from database import Unlisted_Images, Contact_Us_Messages, Newsletter_List, db, app
 from flask import render_template, request, jsonify, redirect
 from tensorflow.lite.python.interpreter import Interpreter
-from database import Database, db, app
 from session import Session
 
 import numpy as np
+import difflib
 import shutil
 import os
 import cv2
@@ -36,7 +37,13 @@ boxes_idx, classes_idx, scores_idx = 1, 3, 0
 
 @app.route('/')
 def index():
-    return render_template('index.html', notification=None)
+    notification = session.get("notification")
+
+    template = render_template('index.html', notification=notification)
+    
+    session.unset("notification")
+
+    return template
 
 @app.route('/detect')
 def detect():
@@ -50,7 +57,7 @@ def result():
 
     file.save(image_path)
 
-    detections = perform_detection(image_path)
+    detections = perform_detection(image_path, "image_detection")
 
     return render_template('result.html', image_filename=file.filename, detections=detections, notification=None)
 
@@ -68,19 +75,24 @@ def unlisted_images():
 
         file.save(image_path)
 
-        data = Database(object_name=object_name)
+        data = Unlisted_Images(object_name=object_name)
 
         data.insert(data)
 
         copy_image(object_name, file.filename)
+
+        perform_detection(image_path, "unlisted_images")
+
+        copy_image_with_detection(object_name, file.filename)
 
         os.remove(image_path)
 
         session.set("notification", {"title": "Success!", "text": "An image has been successfully added to unlisted images.", "icon": "success"})
 
         return redirect("/unlisted_images")
+    
     else:
-        data = db.session.query(Database).group_by(Database.object_name).order_by(Database.date_created.desc()).all()
+        data = db.session.query(Unlisted_Images).group_by(Unlisted_Images.object_name).order_by(Unlisted_Images.date_created.desc()).all()
 
         my_data = None
 
@@ -99,13 +111,94 @@ def unlisted_images():
 def view_images():
     post_image_folder = request.form["folder_name"]
     
-    image_folder = 'static/unlisted_images/' + post_image_folder
+    image_folder = 'static/unlisted_images/with_detections/' + post_image_folder
     filenames = os.listdir(image_folder)
 
     return render_template('view_images.html', filenames=filenames, title=post_image_folder, notification=None)
 
-# Add a new parameter "purpose"
-def perform_detection(image_path):
+@app.route("/archive_folder", methods=["POST"])
+def archive_folder():
+    post_image_folder = request.form["archive_folder_folder_name"]
+
+    db = Unlisted_Images()
+
+    db.delete(post_image_folder)
+
+    source_folder_with_detections = "static/unlisted_images/with_detections/" + post_image_folder
+    source_folder_without_detections = "static/unlisted_images/without_detections/" + post_image_folder
+    
+    destination_folder_with_detections = "static/archived_images/with_detections" 
+    destination_folder_without_detections = "static/archived_images/without_detections" 
+
+    shutil.move(source_folder_with_detections, destination_folder_with_detections)
+    shutil.move(source_folder_without_detections, destination_folder_without_detections)
+
+    session.set("notification", {"title": "Success!", "text": post_image_folder + " folder has been archived.", "icon": "success"})
+
+    return redirect("/unlisted_images")
+
+@app.route("/update_folder_name", methods=["POST"])
+def update_folder_name():
+    post_image_folder = request.form["update_folder_name_folder_name"]
+    post_old_image_folder = request.form["update_folder_name_old_folder_name"]
+
+    db = Unlisted_Images()
+
+    is_record_available = False
+
+    if post_image_folder != post_old_image_folder:
+        is_record_available = db.is_record_available(post_image_folder)
+
+    if not is_record_available:
+        db.update(post_old_image_folder, post_image_folder)
+
+        rename_folder(post_old_image_folder, post_image_folder)
+        rename_folder_with_detections(post_old_image_folder, post_image_folder)
+
+        session.set("notification", {"title": "Success!", "text": post_old_image_folder + " folder has been renamed to " + post_image_folder + ".", "icon": "success"})
+    else:
+        session.set("notification", {"title": "Oops...", "text": post_image_folder + " folder is already in the record.", "icon": "error"})
+
+    return redirect("/unlisted_images")
+
+@app.route("/view_images_with_detections", methods=["POST"])
+def view_images_with_detections():
+    post_image_folder = request.form["folder_name"]
+    
+    image_folder = 'static/unlisted_images/with_detections/' + post_image_folder
+    filenames = os.listdir(image_folder)
+
+    return render_template('view_images_with_detections.html', filenames=filenames, title=post_image_folder, notification=None)
+
+@app.route('/contact_us_message', methods=['POST'])
+def contact_us_message():
+    contact_us_name = request.form["contact_us_name"]
+    contact_us_email = request.form["contact_us_email"]
+    contact_us_subject = request.form["contact_us_subject"]
+    contact_us_message = request.form["contact_us_message"]
+
+    data = Contact_Us_Messages(name=contact_us_name, email=contact_us_email, subject=contact_us_subject, message=contact_us_message)
+    
+    data.insert(data)
+
+    session.set("notification", {"title": "Success!", "text": "Your message has been sent!", "icon": "success"})
+
+    return redirect("/#contact")
+
+@app.route('/newsletter_list', methods=['POST'])
+def newsletter_list():
+    newsletter_email = request.form["newsletter_email"]
+
+    data = Newsletter_List(email=newsletter_email)
+    
+    data.insert(data)
+
+    session.set("notification", {"title": "Success!", "text": "Thank you for subscribing to our newsletter.", "icon": "success"})
+
+    return redirect("/#contact")
+
+def perform_detection(image_path, page):
+    # ================ Start Image Preprocessing ================ #
     image = cv2.imread(image_path)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     imH, imW, _ = image.shape 
@@ -116,40 +209,64 @@ def perform_detection(image_path):
 
     if floating_model:
         input_data = (np.float32(input_data) - input_mean) / input_std
+    # ================ End Image Preprocessing ================ #
 
+    # ================ Start Tensorflow Interpreter ================ #
     interpreter.set_tensor(input_details[0]['index'],input_data)
     interpreter.invoke()
+    # ================ Start Tensorflow Interpreter ================ #
 
+    # ================ Start Initialization of Detection Process ================ #
     boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0]
     classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]
     scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]
+    # ================ End Initialization of Detection Process ================ #
 
     detections = []
 
+    # ================ Start Detection Process ================ #
     for i in range(len(scores)):
         if ((scores[i] > 0.5) and (scores[i] <= 1.0)):
+            # ================ Start Detect Bounding Boxes of Trained Images ================ #
             ymin = int(max(1,(boxes[i][0] * imH)))
             xmin = int(max(1,(boxes[i][1] * imW)))
             ymax = int(min(imH,(boxes[i][2] * imH)))
             xmax = int(min(imW,(boxes[i][3] * imW)))
+            # ================ End Detect Bounding Boxes of Trained Images ================ #
             
+            # ================ Start Intialize Bounding Box to ROI (Region of Interest) ================ #
             cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+            # ================ End Intialize Bounding Box to ROI (Region of Interest) ================ #
 
+            # ================ Start Intialize Labeling the ROI ================ #
             object_name = labels[int(classes[i])]
             label = '%s: %d%%' % (object_name, int(scores[i]*100))
             labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
             label_ymin = max(ymin, labelSize[1] + 10)
+            # ================ End Intialize Labeling the ROI ================ #
             
+            # ================ Start Labeling to ROI (Region of Interest) ================ #
             cv2.rectangle(image, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
             cv2.putText(image, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
+            # ================ End Labeling to ROI (Region of Interest) ================ #
 
+            # ================ Start Set all detection matrix to "detections" variable to be used in the webpage display ================ #
             detections.append({'object_name': object_name, 'confidence': int(scores[i] * 100), 'xmin': xmin, 'ymin': ymin, 'xmax': xmax, 'ymax': ymax})
+            # ================ End Set all detection matrix to "detections" variable to be used in the webpage display ================ #
+    # ================ Start Detection Process ================ #
+
+    if page == "image_detection":
+        base_dir = "static/uploads"
+    else:
+        base_dir = "static/uploads/temp"
 
     image_fn = os.path.basename(image_path)
-    image_savepath = os.path.join(os.getcwd(), "static/uploads", image_fn)
+    image_savepath = os.path.join(os.getcwd(), base_dir, image_fn)
 
+    # ================ Start Save Image to the selected folder ================ #
     cv2.imwrite(image_savepath, image)
-    
+    # ================ End Save Image to the selected folder ================ #
+
     return detections
 
 def copy_image(filename, filepath):
@@ -181,6 +298,71 @@ def copy_image(filename, filepath):
     new_image_name = f"image_{image_count:04d}.jpg"
 
     shutil.copy("static/uploads/temp/" + filepath, os.path.join(folder_path, new_image_name))
+
+def copy_image_with_detection(filename, filepath):
+    base_dir = "static/unlisted_images/with_detections"
+
+    found_folder = False
+
+    for root, dirs, files in os.walk(base_dir):
+        for dir_name in dirs:
+            if dir_name == filename:
+                found_folder = True
+                folder_path = os.path.join(root, dir_name)
+                break
+
+    if not found_folder:
+        folder_path = os.path.join(base_dir, filename)
+        os.makedirs(folder_path)
+    
+    image_count = 0
+    
+    for _, _, files in os.walk(folder_path):
+        for file in files:
+            if file.startswith("image_"):
+                index = int(file.split("_")[-1].split(".")[0])
+                image_count = max(image_count, index)
+
+    image_count += 1
+    
+    new_image_name = f"image_{image_count:04d}.jpg"
+
+    shutil.copy("static/uploads/temp/" + filepath, os.path.join(folder_path, new_image_name))
+
+def similar(a, b):
+    return difflib.SequenceMatcher(None, a, b).ratio()
+
+def rename_folder(old_folder_name, new_folder_name):
+    root_dir = "static/unlisted_images/without_detections"
+
+    for folder_name in os.listdir(root_dir):
+        folder_path = os.path.join(root_dir, folder_name)
+        
+        if os.path.isdir(folder_path):
+            similarity = similar(old_folder_name, folder_name)
+            
+            if similarity >= 0.6:
+                new_folder_path = os.path.join(root_dir, new_folder_name)
+        
+                os.rename(folder_path, new_folder_path)
+
+                return
+
+def rename_folder_with_detections(old_folder_name, new_folder_name):
+    root_dir = "static/unlisted_images/with_detections"
+
+    for folder_name in os.listdir(root_dir):
+        folder_path = os.path.join(root_dir, folder_name)
+        
+        if os.path.isdir(folder_path):
+            similarity = similar(old_folder_name, folder_name)
+            
+            if similarity >= 0.6:
+                new_folder_path = os.path.join(root_dir, new_folder_name)
+        
+                os.rename(folder_path, new_folder_path)
+
+                return
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
